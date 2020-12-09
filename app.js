9e944af27dd1839635cee35621c3db15b5705980
dginovker <dcress01@uoguelph.ca>
2020-12-08 21:32:28 -0500

    Add skeleton highscores route

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var defaultRouter = require('./routes/index');
+let highscoresRouter = require('./routes/highscores');
 
 var app = express();
 
@@ -19,6 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', defaultRouter);
+app.use('/highscores', highscoresRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/highscores.js
+++ routes/highscores.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var router = express.Router();
+let config = require("../config");
 
 router.get('/', function(req, res, next) {
   res.json({ existing_routes: [
-    "/",
-    "highscores/"
+    "/config",
+    ""
   ] });
+});
+
+router.get('/config', function(req, res, next) {
+  res.json(config);
 });
 
 module.exports = router;
